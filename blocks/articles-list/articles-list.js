57bc2ed7c6ad55425aa0660ad5fecab0144fb56c
schagalli <schagalli@adobe.com>
2025-02-14 17:05:56 +0530

    Article links implementation on all articles

--- blocks/articles-list/articles-list.js
+++ blocks/articles-list/articles-list.js
@@ -1,10 +1,58 @@
 export default async function decorate(block) {
-    const articles=block.querySelector('a[href$=".json"]');
-    console.log(articles);
+    const articleLink = block.querySelector('a[href$=".json"]');
+    const response = await fetch(new URL(articleLink.href).pathname);
+    const magazineArticles = await response.json();
+    const noImageAttr = block.classList.contains('no-image');
+    const imageAttr = block.classList.contains('with-image');
 
-    const path = new URL(articles.href);
-    console.log(path);
+    block.innerHTML = '';
 
-    const response = await fetch(path);
-    console.log(response.json);
+    const articlesList = document.createElement('ul');
+    articlesList.classList.add('articles-items-list');
+
+    magazineArticles.data.forEach((article) => {
+        if (article.path && article.path.startsWith('/magazine') && !article.path.endsWith('/magazine/')) {
+            const articleItem = document.createElement('li');
+            articleItem.classList.add('article-item');
+
+            const articleTitle = document.createElement('a');
+            articleTitle.href = article.path;
+
+            if (noImageAttr) {
+                const titleSpan = document.createElement('span');
+                titleSpan.textContent = article.title;
+
+                const lastModified = document.createElement('span');
+                lastModified.classList.add('last-modified-date');
+                lastModified.textContent = formatDate(article.lastModified);
+
+                articleTitle.append(titleSpan, lastModified);
+            } else if (imageAttr) {
+                articleTitle.textContent = article.title;
+
+                const thumbnail = document.createElement('img');
+                thumbnail.src = article.image;
+
+                const description = document.createElement('p');
+                description.classList.add('article-description');
+                description.textContent = article.description;
+
+                articleItem.append(thumbnail, description);
+            }
+
+            articleItem.append(articleTitle);
+            articlesList.append(articleItem);
+        }
+    });
+
+    block.append(articlesList);
+}
+
+function formatDate(lastModified) {
+    const date = new Date(lastModified * 1000);
+    const options = { weekday: 'long', year: 'numeric', month: 'short', day: 'numeric' };
+    const formattedDate = date.toLocaleDateString('en-US', options);
+    const [weekday, monthDay, year] = formattedDate.split(', ');
+    const [month, day] = monthDay.split(' ');
+    return `${weekday}, ${day} ${month} ${year}`;
 }
